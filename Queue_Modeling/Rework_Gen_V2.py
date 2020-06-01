2d26772a8181959b4f174f18252dd659517096ad
Gaurav Sharma <sharmagaurav459@gmail.com>
2020-06-01 03:13:11 -0700

    Update Rework_Gen_V2.py

--- Queue_Modeling/Rework_Gen_V2.py
+++ Queue_Modeling/Rework_Gen_V2.py
@@ -9,7 +9,6 @@ Created on Sun May 24 01:16:56 2020
 import pandas as pd
 import numpy as np
 import random
-#import bisect
 import matplotlib.pyplot as plt
 
 NUM_PATIENTS = 50
@@ -223,22 +222,16 @@ while(clock<10000):
     for x  in pat_data:
         if x["Arrival_Time"]==clock:
             no_val += 1
-            
-    #print("len(service_queue):", len(service_queue))
-    #print("min_depart_time:", min(depart_time))
+
     while(no_val>0):   
          
         depart_non_zero_count = np.count_nonzero(depart_time)
         if depart_non_zero_count != 0:
             min_nonzero = min([num for num in depart_time if num !=0])
-            #print("min_nonzero:", min_nonzero)
-        # print("min_setup_time", min(setup_depart))
-        # print("len(service_queue)",len(service_queue))
             
         """Case_1"""    
         
         if(clock!=0 and clock == min_nonzero and len(service_queue)==0):
-            #and val == False
             
             print("Clock:", clock)
             print("Entering_1\n")
@@ -247,15 +240,10 @@ while(clock<10000):
             num_in_service -= 1
             num_completed += 1
             
-            # if val["is_rework"] in ("Yes"):
-            #     num_in_rework += 1
-            
             depart_index = depart_time.index(clock)
             depart_time[depart_index] = 0
             
-            #print("setup_depart[setup_index]", setup_depart[setup_index])
             print("depart_time[depart_index]", depart_time[depart_index])
-            #print("operator_state:", operator_state)
             
             """Operator state tracking"""
             #busy- 1 and idle- 0
@@ -272,8 +260,6 @@ while(clock<10000):
             print("operator_state:", operator_state)
             
             OP_state = Average(operator_state)
-            
-            #OP_bt = np.count_nonzero(operator_state)
             
             OP_bt = (np.count_nonzero(operator_state)/len(operator_state))*100
             
@@ -295,8 +281,6 @@ while(clock<10000):
             
             MC_state = Average(machine_state)
             
-            #MC_bt = np.count_nonzero(machine_state)
-            
             MC_bt = (np.count_nonzero(machine_state)/len(machine_state))*100
             
             
@@ -305,11 +289,6 @@ while(clock<10000):
             print("Service Depart Time Array: ",depart_time)
             #print("Patient Number: ", val["Pat_no"])
             print("*******************************")
-            
-            #Calling_Population = NUM_PATIENTS - num_in_system - num_completed
-            
-            
-            
             
             Event_info = {#"Patient": val["Pat_no"], 
                   "Event_time" : clock, 
@@ -345,18 +324,10 @@ while(clock<10000):
                 print("Entering_2_A\n")
                 
                 setup_index = setup_depart.index(min(setup_depart))
-                print("setup_index:" , setup_index)
-                
                 depart_index = depart_time.index(min(depart_time))
-                print("depart_index:" , depart_index)
-                
-                setup_depart[setup_index] = clock + val["setup_time"]
-                print("setup_depart[setup_index]", setup_depart[setup_index])
-                
+                setup_depart[setup_index] = clock + val["setup_time"]           
                 depart_time[depart_index] =  setup_depart[setup_index] + val["service_time"]
-                print("depart_time[depart_index]", depart_time[depart_index])
-                
-                
+
                 """Operator state tracking"""
                 #busy- 1 and idle- 0
                 operator_state = []
@@ -369,12 +340,8 @@ while(clock<10000):
                         state = 0
                         operator_state.append(state)
                         
-                print("operator_state:", operator_state)
-                
                 OP_state = Average(operator_state)
-                
-                #OP_bt = np.count_nonzero(operator_state)
-                
+
                 OP_bt = (np.count_nonzero(operator_state)/len(operator_state))*100
                 
                 """Machine state tracking"""
@@ -388,21 +355,9 @@ while(clock<10000):
                     else:
                         state = 0
                         machine_state.append(state)
-                        
-                print("machine_state:", machine_state)
-                
-                MC_state = Average(machine_state)
-                
-                #MC_bt = np.count_nonzero(machine_state)
-                
+
+                MC_state = Average(machine_state) 
                 MC_bt = (np.count_nonzero(machine_state)/len(machine_state))*100
-                
-    # =============================================================================
-    #             if clock == min(depart_time):
-    #                 num_in_service -= 1
-    #                 num_in_system -= 1
-    #                 num_completed -= 1
-    # =============================================================================
                     
                 #else:
                 num_in_service += 1
@@ -420,23 +375,17 @@ while(clock<10000):
                 print("Patient Number: ", val["Pat_no"])
                 print("Test Status: ",Test_Result)
                 print("*******************************")
-                
-    
-                
+
                 if Test_Result in ("Sample Rejected","Rejected in LF and HF Both"):
                     
                     print("Adding Rework into the Arrivals")
-                    
                     new_arrival = depart_time[depart_index] + Test_time
-                    
                     rework_dict = {}
-                    
                     rework_dict["Pat_no"] = val["Pat_no"]
                     rework_dict["Arrival_Time"] = new_arrival
                     rework_dict["service_time"] = val["service_time"]
                     rework_dict["setup_time"] = val["setup_time"]
                     rework_dict["is_rework"] = "Yes"
-                    
                     rework_df.append(rework_dict)
                     
                     #val["Arrival_Time"] = depart_time[depart_index] + Test_time
@@ -447,12 +396,8 @@ while(clock<10000):
                     print("Arrival Value:", new_arrival)
                     print("****************")
                     
-                    print("rework:", rework_dict)
-                    
                     pat_data.append(rework_dict)
-                    
-                    print("pat_data_updated:", pat_data)
-                
+
                 Event_info = {"Patient": val["Pat_no"], 
                               "Arrival_time": val["Arrival_Time"],
                               "Event_time" : clock, 
@@ -479,14 +424,10 @@ while(clock<10000):
                               "Test_Reseult": Test_Result}
                 
                 print("Event_info:\n", Event_info)
-                
                 Event_calendar.append(Event_info)
                 results.append(Event_info)
-                
                 Queue_track.append(queue_level)
-                    
-            
-            
+           
             else:
                 
                 print("Clock:", clock)
@@ -494,14 +435,8 @@ while(clock<10000):
                 print("Entering 2_B\n")
                 service_queue.append(val)
                 
-                print("service_queue:" , service_queue)
-                
-                #num_in_service += 1
                 num_in_system += 1
-                
                 queue_level = len(service_queue)
-                print("queue_level:", queue_level)
-            
                 Queue_track.append(queue_level)
                 
                 if val["is_rework"] in ("Yes"):
@@ -525,47 +460,24 @@ while(clock<10000):
                               "MC_state" : MC_state,
                               "MC_B(t)" : MC_bt} 
                 
-                print("Event_info:\n", Event_info)
-                
+                print("Event_info:\n", Event_info)    
                 Event_calendar.append(Event_info)
-     
-        
+
         """Case_3"""
         
         if(clock>=min(depart_time) and clock >= min(setup_depart) and len(service_queue)!=0):
-        
-            
+ 
             print("Clock:", clock)
             print("Entering_3\n")
             
             val = service_queue.pop(0)
-            print("val_now:", val)
-            print("service_queue:", service_queue)
-            
             queue_level = len(service_queue)
-            print("queue_level:", queue_level)
-        
             Queue_track.append(queue_level)
-            
             setup_index = setup_depart.index(min(setup_depart))
-            print("setup_index:" , setup_index)
-            
             depart_index = depart_time.index(min(depart_time))
-            print("depart_index:" , depart_index)
-            
             setup_depart[setup_index] = clock +  val["setup_time"]
-            print("setup_depart[setup_index]", setup_depart[setup_index])
-    
             depart_time[depart_index] =  setup_depart[setup_index] + val["service_time"]
-            print("depart_time[depart_index]", depart_time[depart_index])
-            
-            # val["setup_depart"] = setup_depart[setup_index]
-            # val["depart_time"] = depart_time[depart_index]
-            
-            # print("changed_val:", val)
-            
-            #status = bool(random.getrandbits(1))
-            
+           
             """Operator state tracking"""
             #busy- 1 and idle- 0
             operator_state = []
@@ -578,8 +490,6 @@ while(clock<10000):
                     state = 0
                     operator_state.append(state)
                     
-            print("operator_state:", operator_state)
-            
             OP_state = Average(operator_state)
             
             OP_bt = (np.count_nonzero(operator_state)/len(operator_state))*100
@@ -595,18 +505,13 @@ while(clock<10000):
                 else:
                     state = 0
                     machine_state.append(state)
-                    
-            print("machine_state:", machine_state)
-            
+
             MC_state = Average(machine_state)
-            
-            #MC_bt = np.count_nonzero(machine_state)
-            
+
             MC_bt = (np.count_nonzero(machine_state)/len(machine_state))*100
     
             num_in_system -= 1
             num_completed += 1
-            
             
             Test_Result = quality_policy(1, yield_selected_mfg, patients_target_bc, val)
             
@@ -616,19 +521,7 @@ while(clock<10000):
             print("Patient Number: ", val["Pat_no"])
             print("Test Status: ",Test_Result)
             print("*******************************")
-            
-    # =============================================================================
-    #         if (val["is_rework"] in ("Yes")):
-    #             rework_df[0]["Departure"] = depart_time[depart_index]
-    #         
-    #         for item in rework_df: 
-    #             rework_check.append(item["Departure"])
-    #         
-    #         if (clock in rework_check):
-    #             num_in_rework -= 1
-    # =============================================================================
-                
-            
+
             if Test_Result in ("Sample Rejected","Rejected in LF and HF Both"):
                 
                 print("Adding Rework into the Arrivals")
@@ -646,9 +539,7 @@ while(clock<10000):
                 rework_dict["is_rework"] = "Yes"
                 
                 rework_df.append(rework_dict)
-                
-                # val["Arrival_Time"] = depart_time[depart_index] + Test_time
-                
+
                 print("***************")
                 print("False Status")
                 # print("Arrival Value:", val["Arrival_Time"])
@@ -656,8 +547,6 @@ while(clock<10000):
                 print("****************")
                 
                 pat_data.append(rework_dict)
-                
-                print("pat_data_updated:", pat_data)
             
             Event_info = {"Patient": val["Pat_no"], "Arrival_time": val["Arrival_Time"],
                           "Event_time" : clock, "Event_Type" : "Departure and Service_Start",
@@ -692,47 +581,15 @@ while(clock<10000):
 
 """Storing Results"""
 
-#print("Full_Event_Calendar:\n" , Event_calendar)
 Event_calendar = pd.DataFrame(Event_calendar)
 Event_calendar['queue'] = Queue_track
 
-# Event_calendar["OP_state"] = sum(Event_calendar["Operator_State"])
-# Event_calendar["MC_state"] = sum(Event_calendar["Machine_State"])
-#Event_calendar["B(t)"] = B_t
 
 Patient_Data = pd.DataFrame(pat_data)
 
 df = pd.DataFrame(results)
-# df["arrival_times"] = Patient_Data["Arrival_Time"]
-#df.insert(1, 'arrival_times', Patient_Data["Arrival_time"])
-# df.insert(4, 'setup_times', setup_time)
-# df.insert(6, 'service_time', service_time)
-#df["waiting_time"]= df["Event_time"]-df["Arrival_time"]
-# #df["Arrival_times"] = arrival_times
 
 """System Visualization"""
-
-#plt.figure()
-# =============================================================================
-# Event_calendar.plot(x='Event_time', y=['queue','Num_in_Service'], 
-#                     kind='area', stacked=False, 
-#                     grid=True, style=['+-','o-'])
-# =============================================================================
-#Event_calendar.plot(x='Event_time', y='Num_in_Service', style='.-', kind='area', stacked=False)
-#plt.show(block= True)
-
-# =============================================================================
-# plt.figure()
-# ax = plt.gca()
-# Event_calendar.plot(x='Event_time', y='queue', linestyle='-', drawstyle='steps', stacked=True, label="% in Queue", ax=ax)
-# Event_calendar.plot(x='Event_time', y='Calling_Population',kind='area', linestyle='-', stacked=True, label="% in Process", color ='pink', ax=ax)
-# Event_calendar.plot(x='Event_time', y='Num_in_Rework',kind='area', linestyle='-', stacked=True, label="% in Rework", ax=ax)
-# Event_calendar.plot(x='Event_time', y='Num_in_System',kind='area', linestyle='-', stacked=True, label="% in System", ax=ax)
-# Event_calendar.plot(x='Event_time', y='Num_Completed',kind='area', linestyle='-', stacked=True, label="% Completed", ax=ax)
-# #Event_calendar.plot(x='Event_time', y='OP_state', kind='area', linestyle='-', style='y', label="Operator State", ax=ax)
-# #Event_calendar.plot(x='Event_time', y='MC_state', kind='area', linestyle='-', label="Machine State", ax=ax)
-# plt.show(block= True)
-# =============================================================================
 
 plt.figure()
 Event_calendar.plot(x='Event_time', y=['queue','Calling_Population', 'Num_in_Rework', 'Num_in_Service', 'Num_Completed'], 
@@ -775,48 +632,13 @@ plt.figure()
 Event_calendar.plot(x='Event_time', y='MC_B(t)', kind='line', linestyle='-', label = 'MC_B(t)')
 plt.show()
 
-
-# =============================================================================
-# Event_calendar["op_1_state"] = Event_calendar["Operator_State"].str[0]
-# Event_calendar["op_2_state"] = Event_calendar["Operator_State"].str[1]
-# Event_calendar["mc_1_state"] = Event_calendar["Machine_State"].str[0]
-# Event_calendar["mc_2_state"] = Event_calendar["Machine_State"].str[1]
-# Event_calendar["mc_3_state"] = Event_calendar["Machine_State"].str[2]
-# 
-# plt.figure()
-# op1_state = Event_calendar.plot(x='Event_time', y='op_1_state', kind= 'line')
-# plt.show()
-# 
-# plt.figure()
-# op2_state = Event_calendar.plot(x='Event_time', y='op_2_state', kind= 'line')
-# plt.show()
-# 
-# plt.figure()
-# mc_1_state = Event_calendar.plot(x='Event_time', y='mc_1_state', kind= 'line')
-# plt.show()
-# 
-# plt.figure()
-# mc_2_state = Event_calendar.plot(x='Event_time', y='mc_2_state', kind= 'line')
-# plt.show()
-# 
-# plt.figure()
-# mc_3_state = Event_calendar.plot(x='Event_time', y='mc_3_state', kind= 'line')
-# plt.show()
-# =============================================================================
-
-
 """System Statistics"""
 
-#Average Queue Level = sum(Queue_Level)/No. of times in queue
 average_queue_level = sum(Queue_track)/len(Queue_track)
 print("average_queue_level :", average_queue_level)
 
 average_people_in_system = sum(Event_calendar['Num_in_System'])/len(Queue_track)
 print("average_people_in_system :", average_people_in_system)
-
-#Average waiting time in queue = Total of times in queue/No. of times in queue
-# avg_wait_time_in_queue = sum(df['Waiting_time'])/np.count_nonzero(Queue_track)
-# print("avg_wait_time_in_queue", avg_wait_time_in_queue)
 
 Time_average_number_in_queue = sum(Queue_track)/max(depart_time)
 print("Time_average_number_in_queue", Time_average_number_in_queue)
